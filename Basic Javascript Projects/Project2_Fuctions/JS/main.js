bb85808f5f8ec763e7abe6e55f91b127e5394d47
Austin313 <32691123+Austin313@users.noreply.github.com>
2021-09-30 14:24:19 -0500

    Functions added, submission push

---
+++ Basic Javascript Projects/Project2_Fuctions/JS/main.js
@@ -0,0 +1,9 @@
+function changeColor(newColor) {
+    var elem = document.getElementById('xmas');
+    elem.style.color = newColor; //this element is where it calls to this function to give a new color,and applys to the value given to it.
+  }
+function myFunction() {
+    var sentence = "I really don't have";
+    sentence += " a lot of money.";
+    document.getElementById("click").innerHTML =sentence; //this is stating the function will print the var sentence, which has been concated
+}
