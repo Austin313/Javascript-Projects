e38684133465763255b5012dd08f1697f8310012
Austin313 <32691123+Austin313@users.noreply.github.com>
2021-09-30 14:05:58 -0500

    first assignment push

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+window.alert("Hello,World"); //saying hello world in pop up
+document.write('Hello world');
+var x = "what the dog doin";
+var x = x.fontcolor("blue");
+document.write(x); 
+var O = "bruh moment"; //pop up window of bruh moment
+window.alert(O);
+document.write("oh no!\" Our table!\" It's broken\"");
+var x = x.fontcolor("blue"); //turns chat blue
+document.write(3+3);
+var T = "he tryna get a mint!"
+document.write(x + T)
