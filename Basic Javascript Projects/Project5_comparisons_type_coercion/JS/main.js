b254c0712c33ab56ec5c30abfdcba28d36afe6d4
Austin313 <32691123+Austin313@users.noreply.github.com>
2021-10-02 13:25:50 -0500

    Update main.js

--- Basic Javascript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic Javascript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,4 +1,5 @@
 document.write(typeof "10" + 5);
+//above is type of ^^^^ and below is function tests vvvvvv//
 function my_Function() {
     document.getElementById("test").innerHTML = 0/0;
     document.getElementById("test2").innerHTML = isNaN('this is a string');
@@ -6,6 +7,7 @@ function my_Function() {
     document.getElementById("test4").innerHTML = !(10 >= 10);
     document.getElementById("test5").innerHTML = !(10 < 1);
 }
+//Below is all the operators
 document.write(3E334);
 document.write(-3E345);
 document.write(19>7);
@@ -14,6 +16,7 @@ console.log(2+2);
 console.log(90<1);
 document.write(10==1);
 document.write(10==10);
+//below i set values and used them for ===
 var A = 10;
 var B = 10;
 var C = "10";
@@ -23,6 +26,7 @@ document.write(A === B);
 document.write(A === E);
 document.write(A === C);
 document.write(C === E);
+//below is the OR and AND operators
 document.write(2>3 && 2 > 4);
 document.write(2<3 && 2 < 4);
 document.write(2<3 || 2 > 4);
